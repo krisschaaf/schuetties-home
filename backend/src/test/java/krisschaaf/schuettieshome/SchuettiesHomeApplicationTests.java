62a37f6fa196252e0986248362925928db88ee86
“krisschaafaclue” <kristoffer.schaaf@aclue.de>
2023-10-08 20:47:49 +0200

    upgrade to java 21

--- backend/src/test/java/krisschaaf/schuettieshome/SchuettiesHomeApplicationTests.java
+++ backend/src/test/java/krisschaaf/schuettieshome/SchuettiesHomeApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SchuettiesHomeApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
