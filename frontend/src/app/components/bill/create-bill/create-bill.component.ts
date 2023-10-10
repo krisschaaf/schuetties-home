13329db92fb0c621ab53ccfe8465b6953c249101
“krisschaafaclue” <kristoffer.schaaf@aclue.de>
2023-10-10 08:17:14 +0200

    refactor

--- frontend/src/app/components/bill/create-bill/create-bill.component.ts
+++ frontend/src/app/components/bill/create-bill/create-bill.component.ts
@@ -45,68 +45,6 @@ export class CreateBillComponent implements OnInit {
     });
   }
 
-  getPhotoSrc(photo: Photo): string {
-    return `data:${photo.type};base64,${photo.data}`;
-  }
-
-  // https://stackoverflow.com/questions/1129216/sort-array-of-objects-by-string-property-value
-  dynamicSort(property: string) {
-    let sortOrder = 1;
-    if (property[0] === "-") {
-      sortOrder = -1;
-      property = property.substring(1);
-    }
-    return (a: any, b: any) => {
-      const result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
-      return result * sortOrder;
-    }
-  }
-
-  loadAvailableCarsOfCustomer(customer: Customer) {
-    this.carService.getCarsByCustomerId(customer.id).subscribe({
-      error: () => {
-        this.notificationService.notifyError();
-      },
-      next: (cars) => {
-        this.availableCars = cars.sort(this.dynamicSort('make'));
-        this.carFormControl.enable();
-      }
-    });
-  }
-
-  addCarToBill(car: Car) {
-    const billedCar: BilledCar = {
-      car: car,
-      endDate: this.todayDate,
-    }
-    this.billedCars.push(billedCar);
-    this.deleteCarFromAvailableCars(car);
-    this.carFormControl.setValue('', {onlySelf: true});
-  }
-
-  changeEndDate(newEndDate: MatDatepickerInputEvent<any, any>, updatedBilledCar: BilledCar) {
-    updatedBilledCar.endDate = newEndDate.value;
-
-    let indexToUpdate = this.billedCars.findIndex(item => item.car.id === updatedBilledCar.car.id);
-    this.billedCars[indexToUpdate] = updatedBilledCar;
-  }
-
-
-  deleteCarFromBill(billedCar: BilledCar) {
-    const index = this.billedCars.findIndex(item => item.car.id == billedCar.car.id);
-    if (index > -1) {
-      this.billedCars.splice(index, 1);
-    }
-    this.availableCars.push(billedCar.car);
-  }
-
-  deleteCarFromAvailableCars(availableCar: Car) {
-    const index = this.availableCars.findIndex(item => item.id == availableCar.id);
-    if (index > -1) {
-      this.availableCars.splice(index, 1);
-    }
-  }
-
   get customerFormControl() {
     return this.createBillForm.get('customerFormControl') as FormControl;
   }
@@ -135,6 +73,72 @@ export class CreateBillComponent implements OnInit {
       : this.paymentAmountFormControl.invalid
       ? 'Der Preis muss z.B. 12.00 sein.'
       : '';
+  }
+
+  onNewCustomerChoosenHandler(customer: Customer) {
+    this.billedCars = [];
+    this.loadAvailableCarsOfCustomer(customer.id);
+  }
+
+  onNewCarChoosenHandler(car: Car) {
+    const billedCar: BilledCar = {
+      car: car,
+      endDate: this.todayDate,
+    }
+    this.billedCars.push(billedCar);
+    this.deleteCarFromAvailableCars(car);
+    this.carFormControl.setValue('', {onlySelf: true});
+  }
+
+  onEndDateChangedHandler(newEndDate: MatDatepickerInputEvent<any, any>, updatedBilledCar: BilledCar) {
+    updatedBilledCar.endDate = newEndDate.value;
+
+    let indexToUpdate = this.billedCars.findIndex(item => item.car.id === updatedBilledCar.car.id);
+    this.billedCars[indexToUpdate] = updatedBilledCar;
+  }
+
+  onCarFromBillDeletedHandler(billedCar: BilledCar) {
+    const index = this.billedCars.findIndex(item => item.car.id == billedCar.car.id);
+    if (index > -1) {
+      this.billedCars.splice(index, 1);
+    }
+    this.availableCars.push(billedCar.car);
+  }
+
+  loadAvailableCarsOfCustomer(customerId: string) {
+    this.carService.getCarsByCustomerId(customerId).subscribe({
+      error: () => {
+        this.notificationService.notifyError();
+      },
+      next: (cars) => {
+        this.availableCars = cars.sort(this.dynamicSort('make'));
+        this.carFormControl.enable();
+      }
+    });
+  }
+
+  deleteCarFromAvailableCars(availableCar: Car) {
+    const index = this.availableCars.findIndex(item => item.id == availableCar.id);
+    if (index > -1) {
+      this.availableCars.splice(index, 1);
+    }
+  }
+
+  getPhotoSrc(photo: Photo): string {
+    return `data:${photo.type};base64,${photo.data}`;
+  }
+
+  // https://stackoverflow.com/questions/1129216/sort-array-of-objects-by-string-property-value
+  dynamicSort(property: string) {
+    let sortOrder = 1;
+    if (property[0] === "-") {
+      sortOrder = -1;
+      property = property.substring(1);
+    }
+    return (a: any, b: any) => {
+      const result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
+      return result * sortOrder;
+    }
   }
 
   buildBill(): BillDTO {
