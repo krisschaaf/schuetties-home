952ce86f0143587a51b95d288afba1da243bb4e8
krisschaaf <kris-schaaf@gmx.de>
2023-10-11 01:43:56 +0200

    add build and push script

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # schuetties-home
+
+### Deployment
+
+1. store static frontend hmtl in spring boot backend ()
+2. 
---
+++ build-and-push-app.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+if [ "$1" = "" ]; then
+    echo "Please enter a commit message"
+    exit 1
+fi
+
+echo "Building Frontend\r\n"
+
+cd frontend && npm run build:prod --output-path=dist 
+
+echo "Copying static files to Backend\r\n"
+
+cp -r dist/* ../backend/src/main/resources/static/
+
+echo "Building Backend\r\n"
+
+cd ../backend && ./gradlew build
+
+echo "Pushing Repo\r\n"
+
+cd .. && git add . && git commit -m "$1" && git push
+
+echo "App pushed\r\n"
