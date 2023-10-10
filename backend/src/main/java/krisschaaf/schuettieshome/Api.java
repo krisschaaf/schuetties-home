e586b2568ea8ed1e46d445d5f2e15974f1e88342
krisschaaf <kris-schaaf@gmx.de>
2023-10-10 19:03:40 +0200

    add auth0 authorization in backend

---
+++ backend/src/main/java/krisschaaf/schuettieshome/Api.java
@@ -0,0 +1,7 @@
+package krisschaaf.schuettieshome;
+
+public interface Api {
+    public static final String CUSTOMERS_PATH = "/customers/customer";
+    public static final String PERMISSION_READ_CUSTOMERS = "SCOPE_read:customers";
+    public static final String PERMISSION_WRITE_CUSTOMERS = "SCOPE_write:customers";
+}
---
+++ backend/src/main/java/krisschaaf/schuettieshome/security/SecurityConfig.java
@@ -0,0 +1,65 @@
+package krisschaaf.schuettieshome.security;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
+import org.springframework.security.oauth2.core.OAuth2TokenValidator;
+import org.springframework.security.oauth2.jwt.*;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Value("${auth0.audience}")
+    private String audience;
+
+    @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
+    private String issuer;
+
+    //https://docs.spring.io/spring-security/reference/servlet/configuration/java.html
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests((authorizeRequests) ->
+                        authorizeRequests.anyRequest().authenticated())
+                .cors((cors) ->
+                        cors.configurationSource(corsConfigurationSource()))
+                .oauth2ResourceServer((oauth2ResourceServer) ->
+                        oauth2ResourceServer.jwt((jwt) -> jwt.decoder(jwtDecoder())))
+                .httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200/"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST", "PUT", "DELETE"));
+        configuration.setAllowedHeaders(Arrays.asList("authorization", "content-type"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+    @Bean
+    JwtDecoder jwtDecoder() {
+        NimbusJwtDecoder jwtDecoder = JwtDecoders.fromOidcIssuerLocation(issuer);
+
+        OAuth2TokenValidator<Jwt> audienceValidator = new AudienceValidator(audience);
+        OAuth2TokenValidator<Jwt> withIssuer = JwtValidators.createDefaultWithIssuer(issuer);
+        OAuth2TokenValidator<Jwt> withAudience = new DelegatingOAuth2TokenValidator<>(withIssuer, audienceValidator);
+
+        jwtDecoder.setJwtValidator(withAudience);
+        return jwtDecoder;
+    }
+}
