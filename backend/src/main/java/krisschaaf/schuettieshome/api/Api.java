4ead27e584fd93cb64fdde92db3735ed158c1d72
krisschaaf <kris-schaaf@gmx.de>
2023-11-02 00:11:16 +0100

    add authorization permissions

--- backend/src/main/java/krisschaaf/schuettieshome/api/Api.java
+++ backend/src/main/java/krisschaaf/schuettieshome/api/Api.java
@@ -7,6 +7,10 @@ public interface Api {
     String PHOTO_PATH = VERSION + "/cars/photo";
     String BILL_PATH = VERSION + "/bills/bill";
 
-//    String PERMISSION_READ_CUSTOMERS = "SCOPE_read:customers";
-//    String PERMISSION_WRITE_CUSTOMERS = "SCOPE_write:customers";
+    String PERMISSION_READ_CUSTOMERS = "SCOPE_read:customers";
+    String PERMISSION_WRITE_CUSTOMERS = "SCOPE_write:customers";
+    String PERMISSION_READ_CARS = "SCOPE_read:cars";
+    String PERMISSION_WRITE_CARS = "SCOPE_write:cars";
+    String PERMISSION_READ_BILLS = "SCOPE_read:bills";
+    String PERMISSION_WRITE_BILLS = "SCOPE_write:customers";
 }
--- backend/src/main/java/krisschaaf/schuettieshome/security/SecurityConfig.java
+++ backend/src/main/java/krisschaaf/schuettieshome/security/SecurityConfig.java
@@ -5,12 +5,15 @@ import krisschaaf.schuettieshome.configuration.SpaWebFilter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
 import org.springframework.security.oauth2.core.OAuth2TokenValidator;
 import org.springframework.security.oauth2.jwt.*;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
@@ -18,6 +21,8 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
+
+import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -34,20 +39,43 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests((authorizeRequests) -> authorizeRequests
-                        .requestMatchers(Api.VERSION + "/**").authenticated())
+                        .requestMatchers(antMatcher(HttpMethod.GET, Api.CUSTOMER_PATH)).hasAuthority(Api.PERMISSION_READ_CUSTOMERS)
+                        .requestMatchers(antMatcher(HttpMethod.POST, Api.CUSTOMER_PATH)).hasAuthority(Api.PERMISSION_WRITE_CUSTOMERS)
+                        .requestMatchers(antMatcher(HttpMethod.PUT, Api.CUSTOMER_PATH)).hasAuthority(Api.PERMISSION_WRITE_CUSTOMERS)
+                        .requestMatchers(antMatcher(HttpMethod.DELETE, Api.CUSTOMER_PATH)).hasAuthority(Api.PERMISSION_WRITE_CUSTOMERS)
+
+                        .requestMatchers(antMatcher(HttpMethod.GET, Api.CAR_PATH)).hasAuthority(Api.PERMISSION_READ_CARS)
+                        .requestMatchers(antMatcher(HttpMethod.POST, Api.CAR_PATH)).hasAuthority(Api.PERMISSION_WRITE_CARS)
+                        .requestMatchers(antMatcher(HttpMethod.PUT, Api.CAR_PATH)).hasAuthority(Api.PERMISSION_WRITE_CARS)
+                        .requestMatchers(antMatcher(HttpMethod.DELETE, Api.CAR_PATH)).hasAuthority(Api.PERMISSION_WRITE_CARS)
+
+                        .requestMatchers(antMatcher(HttpMethod.GET, Api.BILL_PATH)).hasAuthority(Api.PERMISSION_READ_BILLS)
+                        .requestMatchers(antMatcher(HttpMethod.POST, Api.BILL_PATH)).hasAuthority(Api.PERMISSION_WRITE_BILLS)
+                        .requestMatchers(antMatcher(HttpMethod.PUT, Api.BILL_PATH)).hasAuthority(Api.PERMISSION_WRITE_BILLS)
+                        .requestMatchers(antMatcher(HttpMethod.DELETE, Api.BILL_PATH)).hasAuthority(Api.PERMISSION_WRITE_BILLS)
+                        .anyRequest().authenticated())
                 .cors((cors) ->
                         cors.configurationSource(corsConfigurationSource()))
                 .oauth2ResourceServer((oauth2ResourceServer) ->
-                        oauth2ResourceServer.jwt((jwt) -> jwt.decoder(jwtDecoder())))
+                        oauth2ResourceServer.jwt((jwt) -> jwt.decoder(jwtDecoder())
+                                .jwtAuthenticationConverter(jwtAuthenticationConverterWithCustomClaimName("permissions"))))
                 .httpBasic(Customizer.withDefaults())
                 .addFilterAfter(new SpaWebFilter(), BasicAuthenticationFilter.class);
         return http.build();
     }
 
+    private static JwtAuthenticationConverter jwtAuthenticationConverterWithCustomClaimName(String customClaimName) {
+        var jwtGrantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+        jwtGrantedAuthoritiesConverter.setAuthoritiesClaimName(customClaimName);
+        var jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(jwtGrantedAuthoritiesConverter);
+        return jwtAuthenticationConverter;
+    }
+
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("https://schuetties-home.de/"));
+        configuration.setAllowedOrigins(Arrays.asList("https://schuetties-home.de/", "http://localhost:4200/"));
         configuration.setAllowedMethods(Arrays.asList("GET","POST", "PUT", "DELETE"));
         configuration.setAllowedHeaders(Arrays.asList("authorization", "content-type"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
